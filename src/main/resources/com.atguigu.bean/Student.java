8cc181d774dac45fc5f6dcbb140cc6f15903f084
atguiguiybuqun <atguigu2018ybqun@sohu.com>
2021-10-09 08:49:42 +0800

    idea_first_test_conflict_git

--- src/main/resources/com.atguigu.bean/Student.java
+++ src/main/resources/com.atguigu.bean/Student.java
@@ -4,5 +4,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("First Test Git -01");
         System.out.println("Second Test Git-02");
+        System.out.println("Test Conflict with hot-fix");
     }
 }
