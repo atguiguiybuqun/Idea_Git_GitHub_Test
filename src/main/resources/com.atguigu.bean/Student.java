6bf6f2c2542073049a68f44a21010da410121cbe
atguiguiybuqun <atguigu2018ybqun@sohu.com>
2021-10-09 08:47:36 +0800

    idea_second_test_git

--- src/main/resources/com.atguigu.bean/Student.java
+++ src/main/resources/com.atguigu.bean/Student.java
@@ -3,5 +3,6 @@ package com.atguigu.bean;
 public class Student {
     public static void main(String[] args) {
         System.out.println("First Test Git -01");
+        System.out.println("Second Test Git-02");
     }
 }
